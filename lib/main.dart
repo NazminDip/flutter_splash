ccadb86d8033e37c379951323109eb2664630a5f
Nazmin <48346432+NazminDip@users.noreply.github.com>
2022-07-05 16:47:03 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
-
-
 //import 'package:appgradient/first_page.dart';
 
+// ignore_for_file: prefer_const_constructors
+
+import 'package:appgradient/first_page.dart';
 import 'package:flutter/material.dart';
-
-import 'first_page.dart';
-
-
-
-
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home:   FirstPage(),
+home: FirstPage(),
   ));
 }
